61e617cfe814aabb5778d453536e899a78d99ac8
InnaTymoshenko <innatymosh@gmail.com>
2024-07-26 11:34:04 +0300

    Create Cart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,28 @@
+'use client'
+
 import { v4 as uuidv4 } from 'uuid'
 import ProductCard from '@/components/ProductCard'
-import dataProduct from '@/data.json'
+// import dataProduct from '@/data.json'
 import Cart from '@/components/Cart'
+import { useProductCart } from '@/store/store'
 
 export default function Home() {
-	const data: IDessert[] = dataProduct
+	const { product } = useProductCart()
+	// const data: IDessert[] = dataProduct
 
 	return (
 		<main className="container min-h-screen lg:flex-row sx:flex-col">
-			<div className="w-[65%] flex flex-col gap-6">
+			<div className="w-[70%] flex flex-col gap-6">
 				<h1 className="text-4xl font-bold">Desserts</h1>
 				<div className="w-full flex justify-between gap-4 flex-wrap">
-					{data.map(dessert => (
+					{product.map(dessert => (
 						<div key={uuidv4()}>
 							<ProductCard dessert={dessert} />
 						</div>
 					))}
 				</div>
 			</div>
-			<div className="w-[35%] min-h-screen p-2">
+			<div className="w-[30%] min-h-screen p-2">
 				<Cart />
 			</div>
 		</main>
